d5fce56808e3391b9412fd1596bc22afe3b8d060
Jose Angel Velasco <joseangel.velasco@uc3m.es>
2021-04-19 13:13:58 +0200

    updates

--- dummy_project_predictive_modeling.py
+++ dummy_project_predictive_modeling.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-.. module:: main
-   :synopsis: Exploratory Data Analysis of the data set that contains the crimes in the city
-    Data path and other configuration settings are indicated in config.ini
+.. module::Predictive Modelling
+   :synopsis: This script fits a machine learning model to classify whether or not a crime register is a violent or not.
+   First approach is based on a gradient boosted decission trees model
 
 .. moduleauthor:: Jose Angel Velasco - (C) Tessella Spain by Capgemini Engineering - 2021
 
@@ -11,6 +11,7 @@
 """
 # Import dependencies
 import configparser
+import pickle
 # TO DO: Import only sklearn
 import sklearn
 import sklearn.preprocessing
@@ -26,7 +27,7 @@ import dummy_project_utils
 
 def main():
     """
-        **Main program of Crime Data Analysis**
+        **Main program of Crimes Predictive Modelling**
 
         This function execute a descriptive analysis of the crimes data set and provides insight about
         and it can support de decision process related to surveillance schedule
@@ -36,14 +37,17 @@ def main():
     config_object.read('config.ini')
     data_path = config_object._sections['paths']['data_path']
     logs_path = config_object._sections['paths']['logs_path']
-    figures_path = config_object._sections['paths']['figures_path']
+    models_path = config_object._sections['paths']['models_path']
 
     # Initialize logs
-    logger = dummy_project_utils.set_up_logger(path=logs_path)
+    log_file_name = 'predictive_modelling'
+    logger = dummy_project_utils.set_up_logger(path=logs_path + log_file_name)
     logger.info('Initialize logger')
 
+    logger.info('Start Predictive modelling')
+
     # Load data
-    logger.info('Load data')
+    logger.info('Load data from: {}'.format(data_path))
     df = dummy_project_utils.load_data(path=data_path, logger=logger)
     df.reset_index(inplace=True)
 
@@ -61,52 +65,6 @@ def main():
     # Filter by the crimes that have a valid location
     df = df[(df['Location_X'] != 0) & (df['Location_Y'] != 0)]
     df = df[(df['Location_X'] != -1) | (df['Location_Y'] != -1)]
-
-    # Determine frequency of crimes per location
-    df_freq_loc_crimes = dummy_project_utils.get_frequencies(df=df,
-                                                             column='Location',
-                                                             logger=logger)
-    # Extract X and Y coordinates
-    df_freq_loc_crimes['Location_X'] = df_freq_loc_crimes['Location'].apply(
-        lambda x: float(x.strip('()').split(',')[0]))
-    df_freq_loc_crimes['Location_Y'] = df_freq_loc_crimes['Location'].apply(
-        lambda x: float(x.strip('()').split(',')[1]))
-
-    # Plot scatterplot of crimes
-    error = dummy_project_utils.plot_scatterplot(df=df_freq_loc_crimes,
-                                                 var_x='Location_X',
-                                                 var_y='Location_Y',
-                                                 scale='FREQ_CRIMES_PERCENTAGE',
-                                                 path=figures_path + 'crimes_map.png',
-                                                 logger=logger)
-
-    # Obtain some distribution of frequency of occurrence to known how is distributed the crimes
-    # Add results to the logging file
-    logger.info('Obtain distribution of frequency of crimes occurence')
-
-    # Define relevant columns
-    columns = ['OFFENSE_CODE_GROUP', 'REPORTING_AREA', 'DAY_OF_WEEK', 'HOUR', 'MONTH']
-
-    # Iter per columns ang ger the distribution of crimes ocurrence
-    for column in columns:
-        logger.info(column)
-
-        # Get crimes frequencies
-        df_freq = dummy_project_utils.get_frequencies(df=df,
-                                                      column=column,
-                                                      logger=logger)
-
-        # Plot crimes frequencies
-        dummy_project_utils.plot_barplot(df=df_freq[0:20],
-                                         var_x=column,
-                                         var_y='FREQ_CRIMES_PERCENTAGE',
-                                         path=figures_path + 'crimes_distribution_per_{}.png'.format(column),
-                                         logger=logger)
-
-        # report the first 5 values in the log
-        for obs in range(0, 5):
-            logger.info('column value:{} frequency of crimes:{}'.format(df_freq.loc[obs][column],
-                                                                        df_freq.loc[obs]['FREQ_CRIMES_PERCENTAGE']))
 
     # Aggregate historical crime data for each spatial unit (reporting areas) and for each day
     df_aux = df.groupby(['REPORTING_AREA', 'OFFENSE_CODE_GROUP', 'DAY_OF_WEEK'])['index'].count().reset_index() \
@@ -190,16 +148,16 @@ def main():
     # Split input features
     logger.info('Split input features')
     X = df_aux[['REPORTING_AREA', 'OFFENSE_CODE_GROUP', 'DAY_OF_WEEK', 'COUNT_CRIMES']]
-    y = df_aux[['VIOLENT_CRIME_FLAG']]
+    y = df_aux['VIOLENT_CRIME_FLAG'].values
 
     # Determine the ratio of inbalance between violent crimes and non-violent crimes
     df_aux2 = df_aux.groupby(['VIOLENT_CRIME_FLAG'])['VIOLENT_CRIME_FLAG'].count()
 
-    ratio_inbalance = df_aux2.loc[0]/df_aux2.loc[1]
-    logger.info('Ratio inbalance (no. samples label 1/ no. sables plabel 0): {}'.format(ratio_inbalance))
+    ratio_imbalance = df_aux2.loc[0] / df_aux2.loc[1]
+    logger.info('Ratio imbalance (no. samples label 1/ no. sables plabel 0): {}'.format(ratio_imbalance))
 
     # Initialize encoder for input features
-    logger.info('Encode input features')
+    logger.info('Encode input features (label encoder)')
     le = sklearn.preprocessing.LabelEncoder()
 
     # encode input features
@@ -215,33 +173,33 @@ def main():
                                                                                 random_state=42)
 
     # Initalize classification algorithm (gradient boosted decision trees) with hyperparameters manually introduced
-    logger.info('Initialize classification model')
-    gbdt = sklearn.ensemble.GradientBoostingClassifier(loss='deviance',
-                                                       n_estimators=100,
-                                                       learning_rate=1.0,
-                                                       max_depth=1,
-                                                       min_samples_split=2,
-                                                       min_samples_leaf=1,
-                                                       min_weight_fraction_leaf=0,
-                                                       max_features=None,
-                                                       verbose=0,
-                                                       max_leaf_nodes=None,
-                                                       criterion='mse',
-                                                       random_state=0)
+    logger.info('Initialize classification model: {}'.format('GradientBoostingClassifier'))
+    model = sklearn.ensemble.GradientBoostingClassifier(loss='deviance',
+                                                        n_estimators=100,
+                                                        learning_rate=1.0,
+                                                        max_depth=1,
+                                                        min_samples_split=2,
+                                                        min_samples_leaf=1,
+                                                        min_weight_fraction_leaf=0,
+                                                        max_features=None,
+                                                        verbose=0,
+                                                        max_leaf_nodes=None,
+                                                        criterion='mse',
+                                                        random_state=0)
     # Fit model to training data
     logger.info('fit model')
-    gbdt.fit(X=X_train,
-             y=y_train)
+    model.fit(X=X_train,
+              y=y_train)
 
     # Get mean accuracy on the given test data and labels.
     logger.info('calculate accuracy training')
-    accuracy_train = gbdt.score(X=X_train,
-                                y=y_train)
+    accuracy_train = model.score(X=X_train,
+                                 y=y_train)
     logger.info('Train Accuracy: {}'.format(accuracy_train))
 
     # Predict in test
     logger.info('Predict on test dataset')
-    y_hat = gbdt.predict(X=X_test)
+    y_hat = model.predict(X=X_test)
 
     # Calculate confusion matrix
     logger.info('Test confusion matrix')
@@ -266,8 +224,13 @@ def main():
     logger.info('Test AUC ROC: {}'.format(roc))
 
     # Plot test confusion matrix
-    sklearn.metrics.plot_confusion_matrix(gbdt, X_test, y_test)
+    sklearn.metrics.plot_confusion_matrix(model, X_test, y_test)
     matplotlib.pyplot.show()
+
+    # Save model
+    model_file_name = 'clf_model_gbdt.sav'
+    logger.info('Save model in: {}\\{}'.format(models_path, model_file_name))
+    pickle.dump(model, open(model_file_name, 'wb'))
 
     logger.info('Finish')
 
