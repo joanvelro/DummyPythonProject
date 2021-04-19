f60b36d72a084e048d3a7779197431ec3a1e3993
Jose Angel Velasco <joseangel.velasco@uc3m.es>
2021-04-19 12:46:31 +0200

    define predictive model first approach

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Dummy project - Crimes 
-## Exploratory Data Analysis
+## Predictive modelling
 
 * Detect the relationship between the crimes and the different features and characteristics availables
 
@@ -29,5 +29,103 @@
 RUN pip install requirements.txt
 ```
 
+## Approach:
+* Define which crimes are violent (and so more important than others) and which not
+* Binary classification problem to classify new crimes into violent or not
+* First approach with gradient boosted decision trees 
 
-(C) Tessella Spain by Capgemini Engineering - 2021 
+## Offense code groups
+* Manually define wich crimes are violent or not 
+
+| Offense code group   |      Violent      |
+|----------|:-------------:|
+| Fraud|  No | 
+| Investigate Property |    No   | 
+| Property Lost | No |
+
+
+
+* 'Other',no Violent Crime
+* 'Confidence Games'no Violent Crime
+* 'Larceny',no Violent Crime
+  'Auto Theft',no Violent Crime
+* 'Residential Burglary',no Violent Crime
+* 'Violations',,Violent Crime
+  'Harassment',Violent Crime
+* 'Counterfeiting',no Violent Crime
+* 'Larceny From Motor Vehicle',Violent Crime
+* 'Police Service Incidents',Violent Crime
+* 'Investigate Person',Violent Crime
+* 'Recovered Stolen Property',Violent Crime
+* 'Embezzlement',no Violent Crime
+* 'Motor Vehicle Accident Response',Violent Crime
+* 'Simple Assault',Violent Crime
+* 'Warrant Arrests',Violent Crime
+* 'Bomb Hoax',Violent Crime
+* 'Vandalism',Violent Crime
+* 'Missing Person Reported', Violent Crime
+* 'License Plate Related Incidents',no Violent Crime
+* 'Aggravated Assault',Violent Crime
+* 'Property Found',no Violent Crime
+* 'Robbery',Violent Crime
+* 'Restraining Order Violations',Violent Crime
+* 'Property Related Damage',Violent Crime
+* 'Missing Person Located',Violent Crime
+* 'Landlord/Tenant Disputes',Violent Crime
+* 'Disorderly Conduct',Violent Crime
+* 'Auto Theft Recovery', no Violent Crime
+* 'Medical Assistance', no Violent Crime
+* 'License Violation', no Violent Crime
+* 'Towed', no Violent Crime
+* 'Service',no Violent Crime
+* 'Verbal Disputes',Violent Crime
+* 'Liquor Violation', no Violent Crime
+* 'Fire Related Reports', no Violent Crime
+  'Ballistics', Violent Crime
+* 'Evading Fare',No Violent Crime
+* 'Operating Under the Influence',Violent Crime
+* 'Offenses Against Child / Family',Violent Crime
+* 'Drug Violation',Violent Crime
+* 'Other Burglary',No Violent Crime
+* 'Firearm Violations',No Violent Crime
+* 'Commercial Burglary', No Violent Crime
+* 'Search Warrants', No Violent Crime
+* 'Biological Threat', - No Violent Crime
+* 'Harbor Related Incidents',- No Violent Crime
+* 'Firearm Discovery',- Violent Crime
+* 'Prisoner Related Incidents',- Violent Crime
+* 'Homicide',- Violent Crime
+* 'Assembly or Gathering Violations', - Violent Crime
+* 'Manslaughter', - Violent Crime
+* 'Arson', - Violent Crime
+* 'Criminal Harassment',- Violent Crime
+* 'Prostitution',- Violent Crime
+* 'HOME INVASION',- Violent Crime
+* 'Phone Call Complaints',- No violent crime
+* 'Aircraft', - No violent crime
+* 'Gambling', - No violent crime
+* 'INVESTIGATE PERSON', - No violent crime
+* 'Explosives', - Violent Crime
+* 'HUMAN TRAFFICKING', - Violent Crime
+* 'HUMAN TRAFFICKING - INVOLUNTARY SERVITUDE', - Violent Crime
+* 'Burglary - No Property Taken' - No violent crime
+
+
+# References
+* [Kaggle Data Set](https://www.kaggle.com/AnalyzeBoston/crimes-in-boston)
+  
+* [Dataset Boston Police](https://data.boston.gov/dataset/crime-incident-reports-august-2015-to-date-source-new-system
+    )
+  
+* [Analyze Boston](https://data.boston.gov/)
+
+* [Ref 1](https://towardsdatascience.com/understanding-the-confusion-matrix-and-how-to-implement-it-in-python-319202e0fe4d
+  )
+  
+* [Ref 2](https://towardsdatascience.com/demand-forecast-boston-crime-data-64a0cff54820)
+
+* [Ref 3](https://blog.goodaudience.com/taxi-demand-prediction-new-york-city-5e7b12305475
+  )
+  
+
+_(C) Tessella Spain by Capgemini Engineering - 2021_
--- dummy_project_main.py
+++ dummy_project_main.py
@@ -10,10 +10,15 @@
 
 """
 # Import dependencies
-import pandas
-import numpy
 import configparser
+# TO DO: Import only sklearn
+import sklearn
+import sklearn.preprocessing
+import sklearn.metrics
+import sklearn.model_selection
+import sklearn.ensemble
 import matplotlib.pyplot
+import pandas
 
 # Import custom libraries
 import dummy_project_utils
@@ -102,6 +107,169 @@ def main():
         for obs in range(0, 5):
             logger.info('column value:{} frequency of crimes:{}'.format(df_freq.loc[obs][column],
                                                                         df_freq.loc[obs]['FREQ_CRIMES_PERCENTAGE']))
+
+    # Aggregate historical crime data for each spatial unit (reporting areas) and for each day
+    df_aux = df.groupby(['REPORTING_AREA', 'OFFENSE_CODE_GROUP', 'DAY_OF_WEEK'])['index'].count().reset_index() \
+        .rename(columns={'index': 'COUNT_CRIMES'}).sort_values(by=['COUNT_CRIMES'], ascending=False)
+
+    # Group crime types into violent and nonviolent crimes.
+    # 0: no violent crime, 1: violent crime
+    crimes_dict = {
+        'Fraud': 0,
+        'Investigate Property': 0,
+        'Property Lost': 0,
+        'Other': 0,
+        'Confidence Games': 0,
+        'Larceny': 0,
+        'Auto Theft': 0,
+        'Residential Burglary': 0,
+        'Violations': 1,
+        'Harassment': 1,
+        'Counterfeiting': 0,
+        'Larceny From Motor Vehicle': 1,
+        'Police Service Incidents': 1,
+        'Investigate Person': 1,
+        'Recovered Stolen Property': 1,
+        'Embezzlement': 0,
+        'Motor Vehicle Accident Response': 1,
+        'Simple Assault': 1,
+        'Warrant Arrests': 1,
+        'Bomb Hoax': 1,
+        'Vandalism': 1,
+        'Missing Person Reported': 1,
+        'License Plate Related Incidents': 0,
+        'Aggravated Assault': 1,
+        'Property Found': 0,
+        'Robbery': 1,
+        'Restraining Order Violations': 1,
+        'Property Related Damage': 1,
+        'Missing Person Located': 1,
+        'Landlord/Tenant Disputes': 1,
+        'Disorderly Conduct': 1,
+        'Auto Theft Recovery': 0,
+        'Medical Assistance': 0,
+        'License Violation': 0,
+        'Towed': 0,
+        'Service': 0,
+        'Verbal Disputes': 1,
+        'Liquor Violation': 0,
+        'Fire Related Reports': 0,
+        'Ballistics': 1,
+        'Evading Fare': 0,
+        'Operating Under the Influence': 1,
+        'Offenses Against Child / Family': 1,
+        'Drug Violation': 1,
+        'Other Burglary': 0,
+        'Firearm Violations': 0,
+        'Commercial Burglary': 0,
+        'Search Warrants': 0,
+        'Biological Threat': 0,
+        'Harbor Related Incidents': 0,
+        'Firearm Discovery': 1,
+        'Prisoner Related Incidents': 1,
+        'Homicide': 1,
+        'Assembly or Gathering Violations': 1,
+        'Manslaughter': 1,
+        'Arson': 1,
+        'Criminal Harassment': 1,
+        'Prostitution': 1,
+        'HOME INVASION': 1,
+        'Phone Call Complaints': 0,
+        'Aircraft': 0,
+        'Gambling': 0,
+        'INVESTIGATE PERSON': 0,
+        'Explosives': 1,
+        'HUMAN TRAFFICKING': 1,
+        'HUMAN TRAFFICKING - INVOLUNTARY SERVITUDE': 1,
+        'Burglary - No Property Taken': 0
+    }
+
+    # Creates new column with violent crimes flag classification
+    df_aux['VIOLENT_CRIME_FLAG'] = df_aux['OFFENSE_CODE_GROUP'].map(crimes_dict)
+
+    # Split input features
+    logger.info('Split input features')
+    X = df_aux[['REPORTING_AREA', 'OFFENSE_CODE_GROUP', 'DAY_OF_WEEK', 'COUNT_CRIMES']]
+    y = df_aux[['VIOLENT_CRIME_FLAG']]
+
+    # Determine the ratio of inbalance between violent crimes and non-violent crimes
+    df_aux2 = df_aux.groupby(['VIOLENT_CRIME_FLAG'])['VIOLENT_CRIME_FLAG'].count()
+
+    ratio_inbalance = df_aux2.loc[0]/df_aux2.loc[1]
+    logger.info('Ratio inbalance (no. samples label 1/ no. sables plabel 0): {}'.format(ratio_inbalance))
+
+    # Initialize encoder for input features
+    logger.info('Encode input features')
+    le = sklearn.preprocessing.LabelEncoder()
+
+    # encode input features
+    for column in X.columns:
+        le.fit(X[column])
+        X[column + '_ENCODED'] = le.transform(X[column])
+
+    # Split train and test data sets
+    logger.info('Split train-test data sets')
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X.iloc[:, -4:],
+                                                                                y,
+                                                                                test_size=0.33,
+                                                                                random_state=42)
+
+    # Initalize classification algorithm (gradient boosted decision trees) with hyperparameters manually introduced
+    logger.info('Initialize classification model')
+    gbdt = sklearn.ensemble.GradientBoostingClassifier(loss='deviance',
+                                                       n_estimators=100,
+                                                       learning_rate=1.0,
+                                                       max_depth=1,
+                                                       min_samples_split=2,
+                                                       min_samples_leaf=1,
+                                                       min_weight_fraction_leaf=0,
+                                                       max_features=None,
+                                                       verbose=0,
+                                                       max_leaf_nodes=None,
+                                                       criterion='mse',
+                                                       random_state=0)
+    # Fit model to training data
+    logger.info('fit model')
+    gbdt.fit(X=X_train,
+             y=y_train)
+
+    # Get mean accuracy on the given test data and labels.
+    logger.info('calculate accuracy training')
+    accuracy_train = gbdt.score(X=X_train,
+                                y=y_train)
+    logger.info('Train Accuracy: {}'.format(accuracy_train))
+
+    # Predict in test
+    logger.info('Predict on test dataset')
+    y_hat = gbdt.predict(X=X_test)
+
+    # Calculate confusion matrix
+    logger.info('Test confusion matrix')
+    CM = sklearn.metrics.confusion_matrix(y_true=y_test,
+                                          y_pred=y_hat)
+
+    # Get  true positives  / (true positives  + false positives)
+    precision = sklearn.metrics.precision_score(y_true=y_test,
+                                                y_pred=y_hat)
+
+    # Get  ratio true positives / (true positives + false negatives)
+    recall = sklearn.metrics.recall_score(y_true=y_test,
+                                          y_pred=y_hat)
+
+    # get Area Under the Receiver Operating Characteristic Curve (ROC AUC)
+    roc = sklearn.metrics.roc_auc_score(y_true=y_test,
+                                        y_score=y_hat,
+                                        average='macro')
+
+    logger.info('Test Precision: {}'.format(precision))
+    logger.info('Test Recall: {}'.format(recall))
+    logger.info('Test AUC ROC: {}'.format(roc))
+
+    # Plot test confusion matrix
+    sklearn.metrics.plot_confusion_matrix(gbdt, X_test, y_test)
+    matplotlib.pyplot.show()
+
+    logger.info('Finish')
 
 
 if __name__ == "__main__":
